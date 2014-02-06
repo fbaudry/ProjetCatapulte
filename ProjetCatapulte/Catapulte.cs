9bf407f164a45cd78c4d0e2e0493c6edde3af4d1
Remy Rousseaux <rrousseaux@epsi.fr>
2014-02-06 14:26:24 +0100

    Premier Commit du projet
    Creation du projet

---
+++ ProjetCatapulte/Catapulte.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetCatapulte
+{
+    class Catapulte
+    {
+        float hauteurBute;
+        float longueurBras;
+        float masseBras;
+        float angleForceTraction;
+        float masseContrePoid;
+        float masseProjectile;
+        float longueurBase;
+
+        public Catapulte()
+        {
+            Random value = new Random();
+            hauteurBute = value.Next();
+            longueurBras = value.Next();
+            masseBras = value.Next();
+            angleForceTraction = value.Next();
+            masseContrePoid = value.Next();
+            masseProjectile = value.Next();
+            longueurBase = value.Next();
+        }
+
+    }
+}
