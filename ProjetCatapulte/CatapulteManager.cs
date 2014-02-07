9762dba558c42100cd2bf2c448a27485c2c9e0d5
rMz44 <rrousseaux@epsi.fr>
2014-02-07 18:22:19 +0100

    Ajout fonctions catapulteSelectionnee

--- ProjetCatapulte/CatapulteManager.cs
+++ ProjetCatapulte/CatapulteManager.cs
@@ -8,9 +8,33 @@ namespace ProjetCatapulte
 {
     class CatapulteManager
     {
+        
         public static List<Catapulte> selectionCatapultes(List<Catapulte> allCatapultes)
         {
+            List<Catapulte> catapultesSelectionnee = new List<Catapulte>();
 
+            foreach(Catapulte selection in allCatapultes)
+            {
+                double diffNote = 0;
+                double pourcentageAvantage = 0;
+                double avantageCatapulte = 0;
+                double noteMoyenne = selection.noteMoyenne();
+                if( noteMoyenne > Contraintes.noteStandard)
+                {
+                   diffNote = noteMoyenne - Contraintes.noteStandard;
+                   pourcentageAvantage = diffNote / Contraintes.stepPourcentageNote;
+                   avantageCatapulte = pourcentageAvantage * Constantes.NB_CATAPULTE;
+                   for (int i = 0; i <= avantageCatapulte; i++)
+                   {
+                       catapultesSelectionnee.Add(selection);
+                   }
+                }
+                else
+                {
+                    catapultesSelectionnee.Add(selection);
+                }
+            }
+            return catapultesSelectionnee;
         }
 
         //public static List<Catapulte> mutationCatapulte(List<Catapulte> allCatapultes)
